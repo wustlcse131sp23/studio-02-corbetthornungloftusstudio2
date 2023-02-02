941886e5a129fa30f72038cf2f1c7403210b6227
jamescorbett <jamescorbett@localhost>
2023-02-02 15:47:02 -0600

    Studio 2

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,44 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How much money are you starting with?");
+		double startAmount = in.nextDouble();
+		System.out.println("Win probability?");
+		double winChance = in.nextDouble();
+		System.out.println("Win limit?");
+		double winLimit = in.nextDouble();
+		
+		System.out.println("How many days are you playing?");
+		int totalSimulations = in.nextInt();
+		
+		int count = 0;
+		double newAmount = startAmount;
+		System.out.println(startAmount);
+		for(int x = 1; x <= totalSimulations; x++) {
+			
+		while(newAmount > 0.0 && newAmount < winLimit) {
+			//count = 0;
+			double rand = Math.random();
+		if(rand <= winChance) {
+			newAmount++;
+			count++;
+			//Win Result
+			System.out.println(startAmount);
+		} 
+		else {
+			newAmount--;
+			count ++;
+			//Lose Result
+		}
+		System.out.println(newAmount);
+		}
+		System.out.println("Simulation " + x + ": " + count);
+		}
+		
+	}
+}
